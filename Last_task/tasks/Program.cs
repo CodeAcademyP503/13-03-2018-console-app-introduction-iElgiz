5dbd88e7060a78a2a13d2cd7d0b947bfeaeda481
iElgiz <elgiz1998@bk.ru>
2018-03-18 19:57:08 +0400

    Dersler

---
+++ Last_task/tasks/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Tapshirig 1
+
+
+//                    Console.WriteLine("Reqem daxil edin");
+//            int i;
+//            i = Convert.ToInt32(Console.ReadLine());
+//            int a = 1;
+
+            //            while (i >= 10)
+
+            //            {
+            //                i = i / 10;
+            //                a++;
+            //            }
+            //            Console.WriteLine(a);
+
+
+            //            ----------TAHSHIRIG 2-------------- -
+
+            //Console.Write("deyeri girin:");
+            //            int n = Convert.ToInt32(Console.ReadLine());
+            //            int sum = 0;
+            //            while (n != 0)
+            //            {
+            //                sum += n % 10;
+            //                n /= 10;
+            //            }
+            //            Console.WriteLine(sum);
+            //            Console.Read();
+
+            //            ----------TAHSHIRIG 3-------------- -
+
+            //            Console.Write("ifadeni girin: ");
+            //            string ifade = Console.ReadLine();
+            //            string sait = "", samit = "";
+
+            //            ifade = ifade.ToLower();
+
+            //            int sayi = ifade.Length;
+
+            //            for (int i = 0; i <= sayi - 1; i++)
+
+            //            {
+
+            //                char herf = Convert.ToChar(ifade.Substring(i, 1));
+
+            //                if (herf == 'a' || herf == 'i' || herf == 'o' || herf == 'u' || herf == 'e')
+
+            //                {
+
+            //                    sait += herf + "-";
+
+            //                }
+
+            //                else
+
+            //                {
+
+            //                    samit += herf + "-";
+
+            //                }
+
+            //            }
+            //            Console.WriteLine("saitler: " + sait);
+            //            Console.WriteLine("samitler: " + samit);
+            //            Console.Read();
+
+            //            ----------TAHSHIRIG 4-------------- -
+
+            //            string cumle;
+            //            string[] s;
+            //            Console.Write("Cümleni daxil et : ");
+            //            cumle = Console.ReadLine();
+            //            s = cumle.Split(' ');
+            //            Console.WriteLine("==================");
+            //            foreach (string soz in s)
+            //            {
+            //                Console.WriteLine(soz);
+            //            }
+            //            Console.ReadKey();
+
+            //            ----------TAHSHIRIG 5-------------- -
+
+            //            List<string> keywords = new List<string>() { "Tenbel", "Kod yazmayan", "Yola Veren", "Esneyen", "Bekar" };
+
+            //            Console.WriteLine("her hansi bir icumle yazin");
+
+            //            string txt = Console.ReadLine();
+
+
+            //            string[] splitedWords = txt.Split(' ');
+
+
+
+            //            bool checking = false;
+
+            //            foreach (string item in splitedWords)
+            //            {
+
+
+            //                for (int i = 0; i < keywords.Count; i++)
+            //                {
+            //                    for (int z = 0; z < splitedWords.Length; z++)
+            //                    {
+            //                        if (splitedWords[z] == keywords[i])
+            //                        {
+
+            //                            checking = true;
+            //                            break;
+            //                        }
+            //                    }
+
+
+            //                }
+
+
+            //            }
+
+            //            if (checking == true)
+            //            {
+            //                Console.WriteLine("siz losersiniz ");
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("siz loser deyilsiniz ");
+            //            }
+
+            //            ----------TAHSHIRIG 6-------------- -
+
+            //            string change = Console.ReadLine();
+            //            change = change.Replace("tenbel", "Loser");
+            //            Console.WriteLine(change);
+            //            Console.ReadLine();
+
+            //            ----------TAHSHIRIG 7-------------- -
+
+            //            List<int> integer = newList<int>(){ 100, 50, 20, 10, 5, 1 };
+            //            List<string> word = new List<string>() { "Yuzluk", "Ellilik", "Iyirmilik", "Onluq", "Beslik", "Birlik" };
+            //            Console.Write("Ededi yazin: ");
+            //            var eded = Convert.ToInt32(Console.ReadLine());
+            //            for (int i = 0; i < integer.Count; i++)
+            //            {
+            //                if (eded >= integer[i])
+            //                {
+            //                    var reqem = eded / integer[i];
+            //                    eded -= reqem * integer[i];
+            //                    Console.WriteLine(reqem + "-" + word[i]);
+            //                }
+            //            }
+            //            Console.Read();
+        }
+    }
+}
